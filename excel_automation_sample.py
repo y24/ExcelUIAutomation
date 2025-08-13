cde7330d63ba56129c75a0f0bb8f5a02b238fae9
y24 <vintage1022@gmail.com>
2025-08-14 02:03:52 +0900

    fix

--- excel_automation_sample.py
+++ excel_automation_sample.py
@@ -59,12 +59,12 @@ def main():
             # 複数ダイアログを処理する例
             dialog_sequence = [
                 {'title_patterns': ['保存の確認', 'Save As', 'Microsoft Excel'], 'key_action': 's'}, #保存ダイアログでsキーを押下
-                {'title_patterns': ['エラー', 'Error'], 'key_action': '{ENTER}'}  #複数のダイアログが連続して出る場合はこのように記述
+                # {'title_patterns': ['エラー', 'Error'], 'key_action': '{ENTER}'}  #複数のダイアログが連続して出る場合はこのように記述
             ]
             print("保存確認ダイアログの保存ボタンを実行します")
             excel.wait_and_handle_dialogs(dialog_sequence)
             
-            print("処理が完了しました")
+            print("デモが完了しました")
         else:
             print("Excelの起動に失敗しました")
             
